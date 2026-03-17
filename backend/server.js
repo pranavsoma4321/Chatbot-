39cf6bade3c41905357685e3a72f2c722aecbae3
pranav <pranavsoma2912@gmail.com>
2026-03-17 23:36:02 +0530

    removed backend and template files

--- backend/server.js
+++ backend/server.js
@@ -190,6 +190,7 @@ app.get("/assignment/:id", (req, res) => {
 });
 
 // SERVER
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
